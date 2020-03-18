c40580a83d6512fb822a256ccac6ca15e1463c9d
Yeasir H <yeasir01@gmail.com>
2020-03-17 21:52:47 -0700

    addtional changes

--- server.js
+++ server.js
@@ -1,29 +1,51 @@
 // Dependencies
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 const fs = require("fs");
+const dbFile = "./db/db.json";
+const notesData = require(dbFile);
 
 // Sets up the Express App
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+app.get("/api/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, dbFile));
+});
+
+
+app.post("/api/notes", function(req, res) {
+    tableData.push(req.body);
+});
+
+app.post("/api/notes", function(req, res) {
+  let newNote = req.body;
+  newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
+  console.log(newNote);
+  notesData.push(newNote);
+  console.log(newNote);
+  res.json(newNote);
+});
 
 // Starts the server to begin listening
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
